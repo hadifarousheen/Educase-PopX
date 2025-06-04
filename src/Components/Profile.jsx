63a400bfb0e0899735ec85895b00ab117124c527
Hadifa Rousheen <93543742+hadifarousheen@users.noreply.github.com>
2025-06-04 21:25:13 +0530

    change in profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,15 +1,15 @@
 const Profile = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="h-screen w-full  md:w-2/3 border border-gray-400 flex flex-col  px-3 ">
+      <div className="h-screen w-full  md:w-[70%] border border-gray-400 flex flex-col  px-3 ">
         <div className="flex items-center">
           {" "}
-          <h1 className="text-2xl py-6 md:py-8 md:text-6xl ">
+          <h1 className="text-2xl py-6 md:py-8 md:text-5xl ">
             Account Settings
           </h1>
         </div>
 
-        <div className="flex bg-[#f7f8f9] border border-[#f7f8f9] p-4 md:text-5xl">
+        <div className="flex bg-[#f7f8f9] border border-[#f7f8f9] p-4 md:text-4xl">
           <div className="rounded-full">
             <img
               className="rounded-full w-20 h-20 md:w-30 md:h-30"
@@ -21,7 +21,7 @@ const Profile = () => {
             <p>Marry@Gmail.Com</p>
           </div>
         </div>
-        <p className="bg-[#f7f8f9] pb-4 md:text-4xl">
+        <p className="bg-[#f7f8f9] pb-4 md:text-3xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quis
           officiis natus nostrum ut fugiat. Ipsum mollitia a optio numquam.
           Exercitationem, nihil esse.
