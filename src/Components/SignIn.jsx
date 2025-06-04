4c2b0e7d77e329627e275bc78f60a2b3c6ef9dfb
Hadifa Rousheen <93543742+hadifarousheen@users.noreply.github.com>
2025-06-04 21:26:36 +0530

    change in signin

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 const SignIn = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="h-screen w-full  md:w-2/3 border border-gray-400 flex flex-col py-10 px-3 ">
-        <h1 className="font-bold text-3xl mt-6 w-2/3 md:w-full  md:text-6xl">
+      <div className="h-screen w-full  md:w-[70%] border border-gray-400 flex flex-col py-10 px-3 ">
+        <h1 className="font-bold text-3xl mt-6 w-2/3 md:w-full  md:text-5xl">
           Signin to your PopX account
         </h1>
-        <p className="my-4 w-2/3 md:w-full md:text-5xl md:my-6">
+        <p className="my-4 w-2/3 md:w-full md:text-4xl md:my-6">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         </p>
 
-        <fieldset className="border rounded-md md:text-5xl md:rounded-2xl">
+        <fieldset className="border rounded-md md:text-4xl md:rounded-2xl">
           <legend>
             {" "}
             <span className="text-[#6c25ff]">Email Address</span>
@@ -23,7 +23,7 @@ const SignIn = () => {
             className="p-1 text-black m-auto"
           />
         </fieldset>
-        <fieldset className="border my-4 md:my-6 rounded-md md:text-5xl md:rounded-2xl">
+        <fieldset className="border my-4 md:my-6 rounded-md md:text-4xl md:rounded-2xl">
           <legend>
             {" "}
             <span className="text-[#6c25ff]">Password</span>
@@ -35,7 +35,7 @@ const SignIn = () => {
             className="p-1 text-black m-auto"
           />
         </fieldset>
-        <div className="text-center text-white bg-[#cbcbcb] py-2 rounded-md md:text-5xl md:py-4 md:my-2 ">
+        <div className="text-center text-white bg-[#cbcbcb] py-2 rounded-md md:text-4xl md:py-4 md:my-2 ">
           <Link to="/profile">Login</Link>
         </div>
       </div>
